9202f8470d2268383ecd337df361d3d775193122
Jonathansteven21 <jonathansteven.gc@gmail.com>
2022-07-24 00:19:16 -0500

    set up continuous integration

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,12 +4,11 @@ export default defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
+    // solo necesitas agregar estas dos propiedades, no incluir este comentario
+    video: false,
+    screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
-      // modify config values examples
-      // config.defaultCommandTimeout = 10000
-
-      // IMPORTANT return the updated config object
-      return config;
+      // Codigo existente no modificar
     }
   }
 });
