56ae420eaee66fc7f0772bba37f94fb1806e69e7
Jonathan Garavito <92124971+Jonathansteven21@users.noreply.github.com>
2022-07-24 12:49:09 -0500

    Update cypress.config.ts
    Co-authored-by: Daniel Galvis <34688953+danielgalvis98@users.noreply.github.com>

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
   // setupNodeEvents can be defined in either
   // the e2e or component configuration
   e2e: {
-    // solo necesitas agregar estas dos propiedades, no incluir este comentario
     video: false,
     screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
